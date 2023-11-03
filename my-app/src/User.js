3a4d12fb134e5bf08459ded44e21912abeb1bf97
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-11-03 03:08:59 +0000

    removed icon reqiurement

--- my-app/src/User.js
+++ my-app/src/User.js
@@ -3,11 +3,6 @@ import React from 'react'
 function User({title},{icon}) {
   return (
     <div>
-        <div>
-        {/* icon */}
-        {icon}
-        </div>
-
         {/* title */}
         <h2>{title}</h2>
     </div>
