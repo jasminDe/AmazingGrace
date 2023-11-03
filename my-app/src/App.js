5cb982cb94f58500ff8c4d9008f42216ec052e16
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-11-03 03:08:43 +0000

    added user icons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import User from './User';
+import PersonIcon from '@mui/icons-material/Person';
+import DriveEtaIcon from '@mui/icons-material/DriveEta';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
 
 
 function App() {
@@ -9,9 +12,18 @@ function App() {
       <h1>AMAZING GRACE MOBILE TRANSPORT</h1>
       <h3>We're Simply Amazing</h3>
       {/* navigation (passanger/Driver/Dispatcher) */}
-      <User title='Passanger'/>
-      <User title='Driver'/>
-      <User title='Dispatcher'/>
+      <div className='user'>
+        <PersonIcon />
+        <User title='Passanger'/>
+      </div>
+      <div className='user'>
+        <DriveEtaIcon />
+          <User title='Driver'/>
+      </div>
+      <div className='user'>
+        <HeadphonesIcon />
+        <User title='Dispatcher'/>
+      </div>
       {/* About Us */}
       {/* footer (contact us/Join our team) */}
     </div>
