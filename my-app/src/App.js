fb224901d32ad4b1ab08f1483ff5f6f0f35db4bf
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-11-05 23:07:51 +0000

    added class names

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,21 +8,26 @@ import HeadphonesIcon from '@mui/icons-material/Headphones';
 function App() {
   return (
     <div className="App">
-      {/* HEADER */}
-      <h1>AMAZING GRACE MOBILE TRANSPORT</h1>
-      <h3>We're Simply Amazing</h3>
-      {/* navigation (passanger/Driver/Dispatcher) */}
-      <div className='user'>
-        <PersonIcon />
-        <User title='Passanger'/>
+      <div className='header'>
+        <h1>AMAZING GRACE MOBILE TRANSPORT</h1>
+        <h3>We're Simply Amazing</h3>
       </div>
-      <div className='user'>
-        <DriveEtaIcon />
+      
+      <div className='navigationBar'>
+        <div className='user'>
+          <PersonIcon />
+          <User title='Passanger'/>
+        </div>
+
+        <div className='user'>
+          <DriveEtaIcon />
           <User title='Driver'/>
-      </div>
-      <div className='user'>
-        <HeadphonesIcon />
-        <User title='Dispatcher'/>
+        </div>
+
+        <div className='user'>
+          <HeadphonesIcon />
+          <User title='Dispatcher'/>
+        </div>
       </div>
       {/* About Us */}
       {/* footer (contact us/Join our team) */}
