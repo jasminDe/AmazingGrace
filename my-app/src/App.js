fa18f411a7ae08ce462fdf98c74002e21d317430
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-11-05 23:34:03 +0000

    added logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,24 +9,33 @@ function App() {
   return (
     <div className="App">
       <div className='header'>
+      <div className='sub_header'>
+        <img src="AGMT_Logo.png" alt="logo" />
         <h1>AMAZING GRACE MOBILE TRANSPORT</h1>
-        <h3>We're Simply Amazing</h3>
       </div>
-      
+        <h3>We're Simply Amazing</h3> 
+      </div>
+
       <div className='navigationBar'>
         <div className='user'>
+          <button>
           <PersonIcon />
           <User title='Passanger'/>
+          </button>
         </div>
 
         <div className='user'>
+        <button>
           <DriveEtaIcon />
           <User title='Driver'/>
+          </button>
         </div>
 
         <div className='user'>
+        <button>
           <HeadphonesIcon />
           <User title='Dispatcher'/>
+          </button>
         </div>
       </div>
       {/* About Us */}
