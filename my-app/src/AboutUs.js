087188840e8fbd00cc9e0ac9eb8694663f4e8a33
Jasmin DeFino <126633389+jasminDe@users.noreply.github.com>
2023-11-08 01:45:33 +0000

    created about us section

---
+++ my-app/src/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AboutUs({title, discrption}) {
+  return (
+    <div>
+        {title}
+        {discrption}
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default AboutUs
